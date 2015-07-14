c3de8314a3da9036e9fb98993c7fbdf4a1fe9f43
qekudxuv <qekudxuv@gmail.com>
2015-07-14 21:10:56 +0800

    Template

---
+++ Template/Abstract/AbstractMailTemplate.cs
@@ -0,0 +1,61 @@
+﻿namespace DP.Template
+{
+    public abstract class AbstractMailTemplate
+    {
+        protected ISmtpService _smtpSvc = null;
+
+        public AbstractMailTemplate() { }
+
+        public AbstractMailTemplate(ISmtpService smtpSvc)
+        {
+            this._smtpSvc = smtpSvc;
+        }
+
+        /// <summary>
+        /// 設定收件人
+        /// </summary>
+        protected abstract void SetMailReceiver();
+
+        /// <summary>
+        /// 設定附件
+        /// </summary>
+        protected abstract void SetMailAttachment();
+
+        /// <summary>
+        /// 取得主旨
+        /// </summary>
+        /// <returns></returns>
+        protected abstract string GetMailSubject();
+
+        /// <summary>
+        /// 若子類別有需要先行驗證，就覆寫此方法，去決定是否發mail
+        /// </summary>
+        /// <returns></returns>
+        protected virtual bool Verify()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 取得信件內容
+        /// </summary>
+        /// <returns></returns>
+        protected abstract string GetMailContent();
+
+
+        public void SendMail()
+        {
+            if (Verify())
+            {
+                //設定收件人
+                this.SetMailReceiver();
+                //設定附件
+                this.SetMailAttachment();
+                //設定主旨、內容並寄出電子郵件
+                this._smtpSvc.SendMail(
+                            this.GetMailSubject(),
+                            this.GetMailContent());
+            }
+        }
+    }
+}
---
+++ Template/Interface/ISmtpService.cs
@@ -0,0 +1,37 @@
+﻿namespace DP.Template
+{
+    public interface ISmtpService
+    {
+        string Subject { get; set; }
+        bool IsBodyHtml { get; set; }
+        global::System.Net.Mail.MailPriority MailPriority { get; set; }
+
+        void AddAttachment(string attachmentPath);
+        void SetAttachment(string attachmentPath);
+        void ClearAttachment();
+
+        void AddMailTo(string mailAddress);  
+        void AddCcTo(string mailAddress);
+        void AddBccTo(string mailAddress);
+
+        void ClearMailTo();
+        void ClearCcTo();
+        void ClearBccTo();
+
+        void SetMailTo(string mailAddress);
+        void SetCcTo(string mailAddress);
+        void SetBccTo(string mailAddress);
+        
+        string GetMailTo(string sep);        
+        string GetCcTo(string sep);
+        string GetBccTo(string sep);        
+        
+        void SendMail(string content);
+        void SendMail(string subject, string content);
+        void SendMail(string subject, global::System.Text.StringBuilder content);
+        void SendMail(global::System.Text.StringBuilder content);        
+        
+        void SetMailFrom(string fromName, string mailFrom);
+        void SetMailFrom(string mailFrom);  
+    }
+}
