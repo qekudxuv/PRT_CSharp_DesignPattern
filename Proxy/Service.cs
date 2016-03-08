d3e6b0de0b2d15ba646342923f76d8107cbc269f
qekudxuv <qekudxuv@gmail.com>
2016-03-08 12:53:26 +0800

    add proxy pattern

---
+++ Proxy/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.Proxy
+{
+    public class Service : IService
+    {
+        public void Execute()
+        {
+            //do staff
+        }
+    }
+}
---
+++ Proxy/ServiceProxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.Proxy
+{
+    public class ServiceProxy : IService
+    {
+        private IService _service = new Service();
+        public void Execute()
+        {
+            //add log
+            Console.WriteLine("start execution");
+            _service.Execute();
+            Console.WriteLine("end execution");
+        }
+    }
+}
