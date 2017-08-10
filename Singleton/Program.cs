e51e486d6c022e24879f5d4162926fa16aa2aa3f
qekudxuv <qekudxuv@gmail.com>
2017-08-10 20:30:14 +0800

    refactor [Singleton] pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    /// <summary>
+    /// reference from http://www.dotblogs.com.tw/pin0513/archive/2010/03/08/13931.aspx
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.GetInstance().Execute();
+
+            Console.ReadLine();
+        }
+    }
+}
--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -1,20 +1,23 @@
-﻿namespace DP.Singleton
+﻿using System;
+
+namespace Singleton
 {
-    //參考 http://www.dotblogs.com.tw/pin0513/archive/2010/03/08/13931.aspx
     public class Singleton
     {
         //volatile關鍵字，這個關鍵字確保，當uniqueInstance變數被實體化的時候，多個執行緒處理uniqueInstance的作法是正確的。
         private volatile static Singleton _uniqueInstance;
 
+        private static Object _lock = new Object();
+
         // Private constructor prevents instantiation from other classes
         private Singleton() { }
 
-        public static Singleton getInstance()
+        public static Singleton GetInstance()
         {
             //雙重上鎖技巧.(Double-Checked Locking)
             if (_uniqueInstance == null)
             {
-                lock (Singleton._uniqueInstance)
+                lock (_lock)
                 {
                     if (_uniqueInstance == null)
                     {
@@ -23,6 +26,11 @@
                 }
             }
             return _uniqueInstance;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("only one instance in the process");
         }
     }
 }
