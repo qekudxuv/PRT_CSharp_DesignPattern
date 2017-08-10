c606057de9b385f464ec18c1b31fd6f786d6bf6e
qekudxuv <qekudxuv@gmail.com>
2017-08-10 18:33:57 +0800

    add [Observer] pattern

---
+++ Observer/Observer/Implement/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Observer
+{
+    public class Customer : IObserver
+    {
+        public string MyName { private get; set; }
+
+        public Customer(string pName)
+        {
+            MyName = pName;
+        }
+
+        // 更新最新消息
+        public void Update(string pMessage)
+        {
+            Console.WriteLine("   {0} receive a new message:{1}", MyName, pMessage);
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Observer
+{
+    /// <summary>
+    /// reference from https://dotblogs.com.tw/joysdw12/series/1?qq=Web%2520API
+    /// 報社 - ISubject - NewspaperOffice
+    /// 訂閱者 - IObserver - Customer
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 產生一間報社
+            NewspaperOffice office = new NewspaperOffice();
+
+            // Arvin 想要訂閱報紙
+            Customer arvin = new Customer("Arvin");
+            office.SubscribeNewspaper(arvin);
+
+            // Jack 想要訂閱報紙
+            Customer jack = new Customer("Jack");
+            office.SubscribeNewspaper(jack);
+
+            // 報社發送了第一則新聞
+            office.SendNewspaper("News One.......");
+
+            // Arvin 不想看報紙了，要退訂
+            office.UnsubscribeNewspaper(arvin);
+
+            // 報社發送了第二則新聞
+            office.SendNewspaper("News Two.......");
+
+            Console.Read();
+        }
+    }
+}
